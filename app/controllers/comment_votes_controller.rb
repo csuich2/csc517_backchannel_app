ea59e037ee0ba5896861cb000bf781d7a67f4ce7
Daniel Franks <defranks3@gmail.com>
2012-09-17 18:48:23 -0400

    Finalized changes for comment voting

--- app/controllers/comment_votes_controller.rb
+++ app/controllers/comment_votes_controller.rb
@@ -47,9 +47,9 @@ class CommentVotesController < ApplicationController
 
     respond_to do |format|
       if @comment_vote.save
-        format.html { redirect_to post_path(Post.find(@comment_vote.post_id)), :notice => 'Comment vote was successfully created.' }
+        format.html { redirect_to post_path(Post.find(@comment_vote.comment.post_id)), :notice => 'Comment vote was successfully created.' }
       else
-        format.html { redirect_to post_path(@comment_vote.post_id), :notice => 'Comment vote was not created.' }
+        format.html { redirect_to post_path(Post.find(@comment_vote.comment.post_id)), :notice => 'Comment vote was not created.' }
       end
     end
   end
