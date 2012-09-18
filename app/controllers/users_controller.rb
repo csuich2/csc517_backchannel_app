a943664448c8cd2ae153535eec1588e6681249fc
Chris Suich <csuich@webassign.net>
2012-09-17 21:10:29 -0400

    Fixed signup to log you in when signup is successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,13 @@ class UsersController < ApplicationController
     if @user.save
       flash[:notice] = "You signed up successfully"
       flash[:color] = "valid"
+      session[:user_id] = @user.id
+      redirect_to :controller => :sessions, :action => :home
     else
       flash[:notice] = "Form is invalid"
       flash[:color] = "invalid"
+      render 'new'
     end
-    render "new"
   end
 
   # DELETE /users/1
