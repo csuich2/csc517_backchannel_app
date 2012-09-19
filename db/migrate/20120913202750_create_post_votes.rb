09a274138738a976d558a16914c2b024bdbe066c
Chris Suich <csuich2@gmail.com>
2012-09-19 18:44:20 -0400

    Fixed an issue with the post_votes migrate file

--- db/migrate/20120913202750_create_post_votes.rb
+++ db/migrate/20120913202750_create_post_votes.rb
@@ -2,7 +2,7 @@ class CreatePostVotes < ActiveRecord::Migration
   def change
     create_table :post_votes do |t|
       t.integer  "user_id"
-      t.integer  "comment_id"
+      t.integer  "post_id"
       t.timestamps
     end
   end
