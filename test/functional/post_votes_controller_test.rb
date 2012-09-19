f346a3d9965b458ce5dd33e1b54a61b8e6b20758
Daniel Franks <defranks3@gmail.com>
2012-09-19 10:24:54 -0400

    Fixed test in post_votes_controller

--- test/functional/post_votes_controller_test.rb
+++ test/functional/post_votes_controller_test.rb
@@ -11,14 +11,15 @@ class PostVotesControllerTest < ActionController::TestCase
     session[:user_id] = @current_user.id
   end
 
-  ## Error is thrown here due to an inability for the find method to function in the test environment.
-  #test "should create post" do
 
-    #assert_difference('PostVote.count') do
-    #  post :create, { post_vote: { user_id: @post_vote.user.id, post_id: @post_vote.post_id } }
-    #end
+  test "should create post" do
 
-    #assert_redirected_to post_path(assigns(@post.id))
-  #end
+    assert_difference('PostVote.count') do
+      post :create, post_id: @post.id
+    end
+
+    assert assigns(:post_vote)
+    assert_redirected_to post_path(@post.id)
+  end
 
 end
