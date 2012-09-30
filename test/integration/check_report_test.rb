cd8076be109da9936eda68c46d769a5ecb5c7912
Nikhil Loya <nikhil_loya1988@yahoo.co.in>
2012-09-30 17:32:51 -0400

    Adding code for non-admin for report and deleting user. Also verification of HTML and table elements.

--- test/integration/check_report_test.rb
+++ test/integration/check_report_test.rb
@@ -1,24 +1,23 @@
 require 'test_helper'
 
 class CheckReportTest < ActionDispatch::IntegrationTest
-  # To change this template use File | Settings | File Templates.
-
+  # check for Admin
   test 'check report' do
     login('test', 'testing')
 
     get_via_redirect '/users'
     assert_equal '/users', path
-
+    # Retrieve the report for User with id 2
     get_via_redirect '/users/votes/2'
     assert_equal '/users/votes/2', path
-
-    post_via_redirect '/users/votes/2'
     assert_response :success
-    assert_equal '/users/votes/2', path
 
-    assert_select "title", {:count => 1}, "Count is not 2"
-    assert_select "h1", {:text => "User's votes"}
-    assert_select "h2", {:text => "User's posts"}
+    # Verifying the HTML elements
+    assert_select "title", {:count => 1}, "Title Count is 1."
+    assert_select "h1", {:count => 1, :text => "User's votes"}, "H1 header Count is 1."
+    assert_select "h2", {:count => 1, :text => "User's posts"}, "H2 header Count is 1."
+
+    # Verifying the User's posts table elements
     assert_select "table" do
       assert_select "tr" do
         assert_select "th", {:text => "Post title"}
@@ -26,6 +25,7 @@ class CheckReportTest < ActionDispatch::IntegrationTest
       end
     end
 
+    # Verifying the User's comments table elements
     assert_select "table" do
       assert_select "tr" do
         assert_select "th", {:text => "Comment text"}
@@ -36,6 +36,23 @@ class CheckReportTest < ActionDispatch::IntegrationTest
     logout
   end
 
+
+    #check for non-admin
+  test 'non-admin not able to check report' do
+    login('test2', 'testing')
+
+    get_via_redirect '/users'
+    assert_not_equal '/users', path
+    assert_equal '/home', path
+    assert_equal 'You are not authorized to view that page.', flash[:notice]
+
+    get_via_redirect '/users/votes/3'
+    assert_not_equal '/users/votes/3', path
+    assert_equal '/home', path
+    assert_equal 'You are not authorized to view that page.', flash[:notice]
+
+  end
+
   def login(username, password)
     get '/logout'
     get '/login'
@@ -43,7 +60,7 @@ class CheckReportTest < ActionDispatch::IntegrationTest
 
     post_via_redirect '/sessions/login_attempt', :username => username, :login_password => password
     assert_equal '/home', path
-    assert_equal 'Welcome, test!', flash[:notice]
+    assert_equal "Welcome, #{username}!", flash[:notice]
   end
 
   def logout
