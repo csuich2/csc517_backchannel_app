29121cf477dfc0980585913faa7582e56998e383
Nikhil Loya <nikhil_loya1988@yahoo.co.in>
2012-10-01 19:52:42 -0400

    Added few functional tests related to assigning/revoking admin access to increase the coverage %

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
   test "should get new" do
     get :new
     assert_response :success
@@ -66,6 +67,47 @@ class UsersControllerTest < ActionController::TestCase
       post :create, :user => {:username => 'newuser', :password => 'password', :password_confirmation => 'password'}
     end
     assert_equal flash[:notice], "You signed up successfully"
+
+  end
+
+  test "make admin" do
+    @user = users(:user1)
+    @current_user = @user
+    session[:user_id] = @current_user.id
+
+    put(:make_admin, 'id' => "2")
+    assert_equal users(:user2).is_admin, true
+  end
+
+
+
+  test "revoke admin" do
+    @user = users(:user1)
+    @current_user = @user
+    session[:user_id] = @current_user.id
+
+    put(:revoke_admin, 'id' => "2")
+    assert_equal users(:user2).is_admin, false
+  end
+
+  test "admin tries to grant admin access to oneself" do
+    @user = users(:user1)
+    @current_user = @user
+    session[:user_id] = @current_user.id
+
+    put(:make_admin, 'id' => "1")
+    assert_redirected_to users_path
+    assert_equal users(:user1).is_admin, true
+  end
+
+  test "admin tries to revoke admin access to oneself" do
+    @user = users(:user1)
+    @current_user = @user
+    session[:user_id] = @current_user.id
+
+    put(:revoke_admin, 'id' => "1")
+    assert_redirected_to users_path
+    assert_equal users(:user1).is_admin, true
   end
 
 end
