33393f59f4591d90753494647004f58b2b01d6fe
Daniel Franks <defranks3@gmail.com>
2012-10-01 17:51:08 -0400

    Committing owner testing updates

--- test/integration/editing_test.rb
+++ test/integration/editing_test.rb
@@ -3,7 +3,7 @@ require_relative 'testing_mixin'
 
 class EditingTest < ActionDispatch::IntegrationTest
   include TestingMixin
-  test 'edit post' do
+  test 'admin edit post' do
     login('test', 'testing')
 
     get_via_redirect '/posts'
@@ -22,8 +22,43 @@ class EditingTest < ActionDispatch::IntegrationTest
     logout
   end
 
-  test 'edit comment' do
+  test 'user edit post' do
+    login('test2', 'testing')
+
+    get_via_redirect '/posts'
+    assert_equal '/posts', path
+
+    get_via_redirect '/posts/1'
+    assert_equal '/posts/1', path
+
+    get_via_redirect '/posts/1/edit'
+    assert_equal '/posts/1/edit', path
+
+    put_via_redirect '/posts/1', :post => { :title => 'Edit Title', :category_id => 1, :text => 'Edit Post Text' }
+    assert_equal '/posts/1', path
+    assert_equal 'Post was successfully updated.', flash[:notice]
+
+    logout
+  end
+
+  test 'admin edit comment' do
     login('test', 'testing')
+
+    get_via_redirect '/posts'
+    assert_equal '/posts', path
+
+    get_via_redirect '/posts/1'
+    assert_equal '/posts/1', path
+
+    put_via_redirect '/posts/1/comments/1', :comment => { :text => 'Edited Text' }
+    assert_equal '/posts/1', path
+    assert_equal 'Comment was successfully updated.', flash[:notice]
+
+    logout
+  end
+
+  test 'user edit comment' do
+    login('test2', 'testing')
 
     get_via_redirect '/posts'
     assert_equal '/posts', path
